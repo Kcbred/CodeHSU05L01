e108bb2ffa3e486cc015c14afcda4446a8572532
LucasW1234 <113708449+LucasW1234@users.noreply.github.com>
2023-02-01 09:51:04 -0800

    Added Overload Constructer and variables added documentation and some accesor and mutators

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -15,7 +15,13 @@ public Rectangle(){
 }
 
     //overloaded constructor (with parameters)
-public Rectangle(double w, double h){
+
+    /**
+     * Creates a rect  with w and h
+     * @param w width of rect
+     * @param h height of rect
+     */
+    public Rectangle(double w, double h){
     width = w;
     height = h;
 
@@ -28,16 +34,60 @@ public Rectangle(double width){
 
     //accessors
 
+    /**
+     *
+     * @return the width of rect
+     */
+    public double getWidth(){
+    return width;
+}
+public double getHeight(){
+        return height;
+}
 
 
     //mutators
+
+    /**
+     * allows change via user
+     * @param newWidth the width to set the rect
+     */
+    public void setWidth(double newWidth){
+        width  = newWidth;
+    }
 
 
     //other methods
 
 
-
-
     //toString method
 
 }
+
+/*public class superHero(){
+    private String name;
+    private Power power;
+
+    public superHero(String n, Power p)
+        name = n;
+        power = new Power(p.getName(), p.getStrength());
+        //use getters as to get string and name for the new power
+    }
+public class Power() {
+        private String name;
+        private int strength;
+
+
+        public Power() {
+            name = "";
+            strength = 0;
+        }
+
+        public class Power(String name, int strength) {
+            name =  s;
+            strength = str;
+            int str;
+            int str = 15;
+            String s = "Dash"
+        }
+ }
